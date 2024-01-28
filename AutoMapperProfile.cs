9035e413234a1644aefe3de9da065393bf8625f6
Natan Amorim <39222731+NatanAmorim@users.noreply.github.com>
2024-01-27 22:19:04 -0300

    Delete things planned for no longer needed REST API

--- AutoMapperProfile.cs
+++ AutoMapperProfile.cs
@@ -1,28 +1,10 @@
 using AutoMapper;
-using gs_server.Models.Professores;
-using gs_server.Dtos.Professores;
-using gs_server.Models.Usuarios;
-using gs_server.Dtos.Usuarios;
-using gs_server.Models.Produtos;
-using gs_server.Dtos.Produtos;
-
 namespace gs_server.Models;
 
 public class AutoMapperProfile : Profile
 {
   public AutoMapperProfile()
   {
-    CreateMap<CreateUsuarioDto, Usuario>();
-    CreateMap<Usuario, ResponseUsuarioDto>();
-    CreateMap<Usuario, ResponseLeanUsuarioDto>();
 
-    CreateMap<CreateProfessorDto, Professor>();
-    CreateMap<Professor, ResponseProfessorDto>();
-    CreateMap<Professor, ResponseLeanProfessorDto>();
-
-    CreateMap<CreateProdutoDto, Produto>();
-    CreateMap<Produto, ResponseProdutoDto>();
-    CreateMap<CreateProdutoVarianteDto, ProdutoVariante>();
-    CreateMap<ProdutoVariante, ResponseProdutoVarianteDto>();
   }
 }
